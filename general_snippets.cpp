1c41a6508f12ebe592cea2341d198e1f60c694b5
Bishmit Regmi <103090191+Bishmit@users.noreply.github.com>
2023-10-21 23:29:42 +0545

    Add files via upload

---
+++ general_snippets.cpp
@@ -0,0 +1,29 @@
+#include<bits/stdc++.h>
+using namespace std;
+#define ll long long
+#define pii pair<int, int>
+#define vi vector<int>
+#define vll vector<long long>
+#define vs vector<string>
+#define vb vector<bool>
+#define vpii vector<pair<int, int>>
+
+#define rep(i, a, b) for (int i = a; i < b; i++)
+#define repr(i, a, b) for (int i = a; i >= b; i--)
+
+#define sortvec(vec) sort(vec.begin(), vec.end())
+#define rsortvec(vec) sort(vec.rbegin(), vec.rend())
+#define sortarr(arr, size) sort(arr, arr + size)
+#define rsortarr(arr, size) sort(arr, arr + size, greater<int>())
+
+#define pb push_back
+#define mp make_pair
+int main(){
+    ios_base::sync_with_stdio(0);
+    cin.tie(0);
+    int T;
+    cin >> T; 
+    while (T--) {
+    }
+    return 0;  
+}
